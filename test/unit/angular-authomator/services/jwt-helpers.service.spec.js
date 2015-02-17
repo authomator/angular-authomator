6e68141198246ed17da3ecf13b7e452059797f37
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-02-17 17:05:57 +0100

    test(services): add unit tests for jwtHelpers.isTokenExpired()

--- test/unit/angular-authomator/services/jwt-helpers.service.spec.js
+++ test/unit/angular-authomator/services/jwt-helpers.service.spec.js
@@ -12,6 +12,22 @@ describe('jwtHelpers', function() {
   };
   var tokenOneEncoded = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEyMzQ1Njc4OTAsIm5hbWUiOiJKdXJnZW4gVmFuIGRlIE1vZXJlIiwiYWRtaW4iOnRydWV9.LIUWACi2-HTrH5aytl0GGVXH2kUorDOpYa2zODQeafs';
 
+  var tokenExpiredDecoded = {
+    "sub": 1234567890,
+    "name": "Jurgen Van de Moere",
+    "admin": true,
+    "exp": 100
+  };
+  var tokenExpiredEncoded = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEyMzQ1Njc4OTAsIm5hbWUiOiJKdXJnZW4gVmFuIGRlIE1vZXJlIiwiYWRtaW4iOnRydWUsImV4cCI6MTAwfQ.TyLjDCcAvsGbF2cAqUkfwDJwS6qyDapuAszTgnHqe1M';
+
+  var tokenUnexpiredDecoded = {
+    "sub": 1234567890,
+    "name": "Jurgen Van de Moere",
+    "admin": true,
+    "exp": 2424189015835
+  };
+  var tokenUnexpiredEncoded = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEyMzQ1Njc4OTAsIm5hbWUiOiJKdXJnZW4gVmFuIGRlIE1vZXJlIiwiYWRtaW4iOnRydWUsImV4cCI6MjQyNDE4OTAxNTgzNX0.q6cI51q04htnQQfemJhbksfnTkR3Q1hC-LZM5jqv1_o';
+
   beforeEach(module('authomator'));
 
   beforeEach(inject(function(_jwtHelpers_){
@@ -29,5 +45,31 @@ describe('jwtHelpers', function() {
     });
 
   });
+
+  describe('#isTokenExpired(tokenWithoutExpiryDate)', function(){
+
+    it('should return false', function(){
+      expect(jwtHelpers.isTokenExpired(tokenOneEncoded)).to.equal(false);
+    });
+
+  });
+
+  describe('#isTokenExpired(unexpiredToken)', function(){
+
+    it('should return false', function(){
+      expect(jwtHelpers.isTokenExpired(tokenUnexpiredEncoded)).to.equal(false);
+
+    });
+
+  });
+
+  describe('#isTokenExpired(expiredToken)', function(){
+
+    it('should return true', function(){
+      expect(jwtHelpers.isTokenExpired(tokenExpiredEncoded)).to.equal(true);
+    });
+
+  });
+
 
 });
